29621f83061a7ae24cb36aa562c66693fd87c3bb
MatteoMer <30910760+MatteoMer@users.noreply.github.com>
2024-06-06 12:31:15 +0200

    basic rlp encoding done

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,56 @@ impl From<&str> for RLPEncoding {
             encoding = Vec::from([0x80]);
         } else if value.len() == 1 && (0x00..0x7f).contains(&value.as_bytes()[0]) {
             encoding = Vec::from([value.as_bytes()[0]])
-        } else if (2..55).contains(value.len()) {
+        } else if (2..55).contains(&value.len()) {
+            let encoding_byte = 0x80 + value.len();
+            encoding = Vec::from([encoding_byte as u8]);
+            encoding.extend_from_slice(value.as_bytes());
+        } else {
+            let mut b_len = Vec::new();
+            let mut x = value.len();
+            while x > 0 {
+                b_len.push((x % 256) as u8);
+                x /= 256;
+            }
+            b_len.reverse();
+            let encoding_byte = 0xb7 + b_len.len();
+            encoding = Vec::from([encoding_byte as u8]);
+            encoding.extend_from_slice(&b_len);
+            encoding.extend_from_slice(value.as_bytes());
         }
         Self { encoding }
     }
 }
 
-impl<T> From<Vec<T>> for RLPEncoding {
+impl<T: Into<RLPEncoding>> From<Vec<T>> for RLPEncoding {
     fn from(list: Vec<T>) -> Self {
         let mut encoding: Vec<u8> = vec![];
 
         if list.is_empty() {
             encoding = Vec::from([0xc0]);
+        }
+
+        let mut concat: Vec<u8> = vec![];
+        for item in list {
+            concat.extend_from_slice(&(item.into()).encoding);
+        }
+
+        let encoding_byte = 0xc0 + concat.len();
+        if concat.len() <= 55 {
+            encoding.extend_from_slice(&[encoding_byte as u8]);
+            encoding.extend_from_slice(&concat);
+        } else {
+            let mut b_len = Vec::new();
+            let mut x = concat.len();
+            while x > 0 {
+                b_len.push((x % 256) as u8);
+                x /= 256;
+            }
+            b_len.reverse();
+            let encoding_byte = 0xf7 + b_len.len();
+            encoding = Vec::from([encoding_byte as u8]);
+            encoding.extend_from_slice(&b_len);
+            encoding.extend_from_slice(&concat);
         }
 
         Self { encoding }
@@ -46,8 +84,13 @@ impl<T> From<Vec<T>> for RLPEncoding {
 fn main() {
     //let input: Vec<u8> = vec![];
     //let input = "";
-    let input = "t";
-    //let input = "test";
+    //let input = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+    //let input = "Bonjour les amis comment ca va, moi ca va super et je suis entrain d'ecrire ca car c'est vraiment super rigolo d'ecrire. Recommencons. Bonjour les amis comment ca va, moi ca va super et je suis entrain d'ecrire ca car c'est vraiment super rigolo d'ecrire lol.";
+    //let input = vec!["bonjour", "les", "amis", "ca", "va", "?"];
+    //let input = vec!["cat", "dog"];
+    let input = vec![
+        "apple", "bread", "cheese", "date", "egg", "fig", "guava", "honey", "ice", "jam", "kale",
+    ];
     println!("[rlp-encoding] starting to encode");
 
     let out = RLPEncoding::from(input);
